ebb5f41f558c107897ad5883958a2afaa617310f
Rodrigo Bervig <rodrigo_bervig@hotmail.com>
2023-01-04 17:08:52 -0300

    Update Floyd Warshall

--- Graphs/floydWarshall.cpp
+++ Graphs/floydWarshall.cpp
@@ -1,34 +1,14 @@
-/**
-*	author:     Bervig
-**/
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define dbg(x) { cerr << #x << " = " << x << "\n"; } 
-#define pb push_back
-typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
-
-//algo to find the distance of every node to every other node
+//find the distance of every node to every other node
 //complexity: O(n^3)
 
+// be careful with not using T_MAX for default values in dist matrix, since it can overflow 
+int n;
+const int MAXN = 450; //less than this and you can use something like N dijkstras
 int dist[MAXN][MAXN];
 
-void floydWarschal(){
+void floydWarshall(){
 	for(int k = 1; k <= n; k++)
 		for(int i = 1; i <= n; i++)
-			for(int j = 1; j <= j++)
+			for(int j = 1; j <=n j++)
 				dist[i][j] = min(dist[i][j], dist[k][i] + dist[k][j]);
-}
-
-int main() {
-
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	return 0;
 }
