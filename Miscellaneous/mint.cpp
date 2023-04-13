0f0eddc0a1cd138f054b6959f50546c82cb852c4
Rodrigo Bervig <rodrigo_bervig@hotmail.com>
2023-04-13 11:35:43 -0300

    added mint and modular arithmetic templates

---
+++ Miscellaneous/mint.cpp
@@ -0,0 +1,117 @@
+using uint = unsigned int;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B) {
+	return (ull)rng() % B;
+}
+ 
+
+const uint MOD = 1000000007;
+template<uint mod = MOD> struct mint { // 1000000007  1000000009
+	uint x;
+ 
+	mint() : x(0) {}
+	mint(ll _x) {
+		_x %= mod;
+		if (_x < 0) _x += mod;
+		x = _x;
+	}
+ 
+	mint& operator += (const mint &a) {
+		x += a.x;
+		if (x >= mod) x -= mod;
+		return *this;
+	}
+	mint& operator -= (const mint &a) {
+		x += mod - a.x;
+		if (x >= mod) x -= mod;
+		return *this;
+	}
+	mint& operator *= (const mint &a) {
+		x = (ull)x * a.x % mod;
+		return *this;
+	}
+	mint pow(ll pw) const {
+		mint res = 1;
+		mint cur = *this;
+		while(pw) {
+			if (pw & 1) res *= cur;
+			cur *= cur;
+			pw >>= 1;
+		}
+		return res;
+	}
+	mint inv() const {
+		assert(x != 0);
+		uint t = x;
+		uint res = 1;
+		while(t != 1) {
+			uint z = mod / t;
+			res = (ull)res * (mod - z) % mod;
+			t = mod - t * z;
+		}
+		return res;
+	}
+	mint& operator /= (const mint &a) {
+		return *this *= a.inv();
+	}
+	mint operator + (const mint &a) const {
+		return mint(*this) += a;
+	}
+	mint operator - (const mint &a) const {
+		return mint(*this) -= a;
+	}
+	mint operator * (const mint &a) const {
+		return mint(*this) *= a;
+	}
+	mint operator / (const mint &a) const {
+		return mint(*this) /= a;
+	}
+ 
+	bool sqrt(mint &res) const {
+		if (mod == 2 || x == 0) {
+			res = *this;
+			return true;
+		}
+		if (pow((mod - 1) / 2) != 1) return false;
+		if (mod % 4 == 3) {
+			res = pow((mod + 1) / 4);
+			return true;
+		}
+		int pw = (mod - 1) / 2;
+		int K = 30;
+		while((1 << K) > pw) K--;
+		while(true) {
+			mint t = myRand(mod);
+			mint a = 0, b = 0, c = 1;
+			for (int k = K; k >= 0; k--) {
+				a = b * b;
+				b = b * c * 2;
+				c = c * c + a * *this;
+				if (((pw >> k) & 1) == 0) continue;
+				a = b;
+				b = b * t + c;
+				c = c * t + a * *this;
+			}
+			if (b == 0) continue;
+			c -= 1;
+			c *= mint() - b.inv();
+			if (c * c == *this) {
+				res = c;
+				return true;
+			}
+		}
+		assert(false);
+	}
+ 
+	bool operator == (const mint &a) const {
+		return x == a.x;
+	}
+	bool operator != (const mint &a) const {
+		return x != a.x;
+	}
+	bool operator < (const mint &a) const {
+		return x < a.x;
+	}
+};
+
